f2fdefcb20e79e636f00e51fad413228e1f76011
caoimhe <caoimhe.gallahue@gmail.com>
2020-10-25 17:19:59 -0700

    started making forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(2),
+      width: '25ch',
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField required id="standard-required" label="First Name" />
+        <TextField required id="standard-required" label="Last Name" />
+        <TextField required id="standard-required" label="Email" type="email" />
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          color="secondary"
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="First Name"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Last Name"
+          variant="outlined"
+        />
+         <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          type="email"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+      </div>
+    </form>
   );
 }
+
 
 export default App;
