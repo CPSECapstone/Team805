34835c755835d59df23dc4f82692a983f1c21ff7
adrien-chaussabel <adrien.chaussabel@gmail.com>
2020-10-27 12:02:02 -0700

    barebones implementation of the uibuilder call to the form data GET from vendor, using fake data for testing

---
+++ backend/uibuilder.js
@@ -0,0 +1,39 @@
+const fakeJson = {
+	"form_name": "name", 
+	"form_desc": "desc",
+	"fields": [{"field_type": "radio", "field_name": "radio1"},
+              {"field_type": "text_input", "field_name": "first_name"},
+              {"field_type": "text_input", "field_name": "last_name"},
+              {"field_type": "email_text_input", "field_name": "email"}]
+}
+
+const getFormData = async() => {
+  const response = await fetch('http://localhost:3000/formData');
+  const myJson = await response.json();
+  return myJson;
+}
+
+const parseFormData = (formData) => {
+  return new Form(formData['form_name'], formData['form_desc'], parseFields(formData['fields']));
+}
+
+const parseFields = (fieldsData) => {
+  return fieldsData.map(field => new Field(field['field_type'], field['field_name']))
+}
+
+class Form {
+  constructor(formName, formDesc, fields) {
+    this.formName = formName;
+    this.formDesc = formDesc;
+    this.fields = fields;
+  }
+}
+
+class Field {
+  constructor(fieldType, fieldName) {
+    this.fieldType = fieldType;
+    this.fieldName = fieldName;
+  }
+}
+
+console.log(parseFormData(fakeJson));
