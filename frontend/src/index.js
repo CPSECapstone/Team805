5d87467a64e018f80e54281c280668d952df666f
Nathan Palmiero <ncpalmie@calpoly.edu>
2020-11-10 17:41:14 -0500

    Moved uibuilder.js to frontend due to React restrictions on imports outside of src

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as uibuilder from './uibuilder';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      loginPost={uibuilder.loginPost}
+      getFormData={uibuilder.getFormData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
