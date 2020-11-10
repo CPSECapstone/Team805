4130e49f19cbe195a10d51a654cef38fd52c0e7d
Nathan Palmiero <ncpalmie@calpoly.edu>
2020-11-09 21:44:25 -0500

    Fixed eslint errors in backend files

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.post('/', (req, res) => {
-  return res.send(`Capstone POST received. Payload was: ${req.headers['message']}`)
+  return res.send(`Capstone POST received. 
+    Payload was: ${req.headers['message']}`);
 });
 
 app.listen(5000, () =>
-  console.log("API started on port 5000"));
+  console.log('API started on port 5000'));
--- backend/uibuilder.js
+++ backend/uibuilder.js
@@ -1,56 +1,75 @@
 const axios = require('axios');
 
 const loginPost = async (userToken, passToken) => {
-  return axios.post("http://localhost:5000/login", 
-  {
-    username: userToken,
-    password: passToken,
-  })
-  .then(post_res => {
-    console.log(post_res.status);
-    console.log("User " + userToken + " successfully logged in.");
-    return post_res.status;
-  })
-  .catch(err => {
-    console.log(err);
-    return null;
-  });
-}
+  return axios.post('http://localhost:5000/login',
+      {
+        username: userToken,
+        password: passToken,
+      })
+      .then((response) => {
+        console.log(response.status);
+        console.log('User ' + userToken + ' successfully logged in.');
+        return response.status;
+      })
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
+};
 
 const getFormData = async () => {
   return axios.get('http://localhost:5000/form')
-  .then(response => {
-    return response;
-  })
-  .catch(error =>
-    console.log(error));
-}
+      .then((response) => {
+        return response;
+      })
+      .catch((error) =>
+        console.log(error));
+};
 
 const parseFormData = (formData) => {
-  return new Form(formData['form_name'], formData['form_desc'], parseFields(formData['fields']));
-}
+  return new Form(formData['form_name'], formData['form_desc'],
+      parseFields(formData['fields']));
+};
 
 const parseFields = (fieldsData) => {
-  return fieldsData.map(field => new Field(field['field_type'], field['field_name']))
-}
+  return fieldsData.map((field) => new Field(field['field_type'],
+      field['field_name']));
+};
 
+/** Form object created from JSON form response */
 class Form {
+  /**
+   * Creates a form object
+   * @param {string} formName - the name of the form
+   * @param {string} formDesc - a short description of the form
+   * @param {Field[]} fields - an array of Field objects
+   */
   constructor(formName, formDesc, fields) {
     this.formName = formName;
     this.formDesc = formDesc;
     this.fields = fields;
   }
 
+  /**
+   * Returns the number of fields in a form
+   * @return {number} - length of fields array attribute
+   */
   numFields() {
     return this.fields.length;
   }
 }
 
+/** Field object representing a single field in a Form */
 class Field {
+  /**
+   * Creates a field object
+   * @param {string} fieldType - type of form element
+   * @param {string} fieldName - name of the field
+   */
   constructor(fieldType, fieldName) {
     this.fieldType = fieldType;
     this.fieldName = fieldName;
   }
 }
 
-module.exports = { loginPost, getFormData, parseFormData };
+module.exports = {loginPost, getFormData, parseFormData};
