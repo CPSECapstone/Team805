0e89c5be02c467376560e21b3666c315263ed5f8
Nathan Palmiero <ncpalmie@calpoly.edu>
2020-12-02 18:21:39 -0500

    Added .travis.yml, testing script, and altered tests on front and backend

--- backend/tests/uibuilder.test.js
+++ backend/tests/uibuilder.test.js
@@ -1,9 +1,9 @@
-const uibuilder = require('../uibuilder');
+const uibuilder = require('../../frontend/src/uibuilder');
 
 // will not pass unless api is running
 test('login should be successful', () => {
   return uibuilder.loginPost('testUser', 'testPassword')
-  .then(data => {
-    expect(data).toBe(200);
-  });
+      .then((data) => {
+        expect(data).toBe(200);
+      });
 });
