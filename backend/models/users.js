bb197d2e1b8f4951adafd9dbe5c648a6660ab721
Jason Shotts <jsshotts@gmail.com>
2021-03-03 20:33:01 -0800

    deleted duplicate schema fields from merge

--- backend/models/users.js
+++ backend/models/users.js
@@ -5,18 +5,6 @@ const users = new Schema({
   userId: {
     type: String,
   },
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
   serviceIds: {
     type: Array,
   },
