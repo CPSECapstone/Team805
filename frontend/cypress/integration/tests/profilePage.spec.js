05d9af4667c317deeafb6b28cfe7a8f1f996f4e9
Nathan Palmiero <ncpalmie@calpoly.edu>
2021-03-11 22:56:54 -0500

    Hopefully last commit

--- frontend/cypress/integration/tests/profilePage.spec.js
+++ frontend/cypress/integration/tests/profilePage.spec.js
@@ -2,8 +2,8 @@ describe('Profile page test', () => {
   it('Verifies profile page pulls correct information', function() {
     // hard coding login to get to restricted route
     cy.visit('localhost:3000/login');
-    cy.get('#username').type('user1');
-    cy.get('#password').type('password!');
+    cy.get('#username').type('myusername');
+    cy.get('#password').type('mypassword');
     cy.contains('Sign In').click();
 
     // test that profile page pulls from db
