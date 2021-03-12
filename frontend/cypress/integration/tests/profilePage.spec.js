e46759a02003512794cfb3b0426025ee92ccf2fd
Nathan Palmiero <ncpalmie@calpoly.edu>
2021-03-11 23:24:11 -0500

    Fixed test script to run all components, added env vars to .travis.yml, fixed cypress tests failing

--- frontend/cypress/integration/tests/profilePage.spec.js
+++ frontend/cypress/integration/tests/profilePage.spec.js
@@ -2,17 +2,17 @@ describe('Profile page test', () => {
   it('Verifies profile page pulls correct information', function() {
     // hard coding login to get to restricted route
     cy.visit('localhost:3000/login');
-    cy.get('#username').type('user1');
-    cy.get('#password').type('password!');
+    cy.get('#username').type('myusername');
+    cy.get('#password').type('mypassword');
     cy.contains('Sign In').click();
 
     // test that profile page pulls from db
     cy.contains('Profile').click();
     cy.url().should('include', '/profile');
     // verify email is pulled
-    cy.get('p').should('contain', 'testuser@gmail.com');
+    cy.get('p').should('contain', 'myusername@gmail.com');
     // verify username is pulled
-    cy.get('p').should('contain', 'testuser');
+    cy.get('p').should('contain', 'myusername');
   });
 });
 
