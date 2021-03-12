89b5594adcdbe2c62fa9dde08bbb60a07e72f098
Nathan Palmiero <ncpalmie@calpoly.edu>
2021-03-11 20:04:04 -0500

    Fixed issues with TravisCI config, tests now all run properly

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-console.log('Backend Server Start');
-
 // Database connection (ensure env variables are set for username/password)
 const db = 'mongodb+srv://' + process.env.dbuser + ':' + process.env.dbpass + '@cloudhaven.92yac.mongodb.net/CloudHaven?retryWrites=true&w=majority';
 mongoose.set('useFindAndModify', false);
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
-
-console.log('Backend Server Mongo Connection Successful');
 
 // Middleware
 const app = express();
@@ -23,7 +19,5 @@ app.use(cors({
 app.use(require('./routes/userRoutes'));
 app.use(require('./routes/serviceRoutes'));
 app.use(require('./routes/authRoutes'));
-
-console.log('Backend PreListen');
 
 app.listen(3001, () => console.log('API on port 3001'));
